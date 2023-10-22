f28be57d084de482bbaff1dcafe7ce2d399de5b7
secangu <carvajalsergio2018@outlook.es>
2023-10-21 19:17:15 -0500

    mas Escenario

--- GameJam+2023/Assets/GameJam/_Scripts/RandomMovemen_sc.cs
+++ GameJam+2023/Assets/GameJam/_Scripts/RandomMovemen_sc.cs
@@ -2,22 +2,18 @@ using UnityEngine;
 
 public class RandomMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float maxSpeed = 2.0f; // Velocidad máxima del objeto.
+    [SerializeField] float maxSpeed;
 
-    [SerializeField]
-    private float changeDirectionTime = 2.0f; // Frecuencia de cambio de dirección.
+    [SerializeField] float changeDirectionTime; 
 
-    [SerializeField]
-    private Transform limitObject; // Objeto que representa el límite de movimiento.
-    [SerializeField] Vector2 limitObjectSize; // Tamaño del límite del objeto.
+    [SerializeField] Transform limitObject; 
+    [SerializeField] Vector2 limitObjectSize; 
 
-    private Vector2 currentDirection;
-    private float timeUntilChange;
+    Vector2 currentDirection;
+    float timeUntilChange;
 
     private void Start()
     {
-        // Inicializa la dirección aleatoria y el tiempo hasta el próximo cambio.
         CambiarDireccion();
     }
 
@@ -26,21 +22,18 @@ public class RandomMovement : MonoBehaviour
         Vector2 movement = currentDirection * maxSpeed * Time.deltaTime;
         Vector2 newPosition = (Vector2)transform.position + movement;
 
-        // Verifica si la nueva posición está dentro de los límites del área.
         if (newPosition.x < limitObject.position.x - limitObjectSize.x || newPosition.x > limitObject.position.x + limitObjectSize.x)
         {
-            currentDirection.x = -currentDirection.x; // Invierte la dirección en el eje X.
+            currentDirection.x = -currentDirection.x; 
         }
         if (newPosition.y < limitObject.position.y - limitObjectSize.y || newPosition.y > limitObject.position.y + limitObjectSize.y)
         {
-            currentDirection.y = -currentDirection.y; // Invierte la dirección en el eje Y.
+            currentDirection.y = -currentDirection.y;
         }
 
-        // Aplica la corrección de dirección y actualiza la posición.
         movement = currentDirection * maxSpeed * Time.deltaTime;
         transform.Translate(movement);
 
-        // Verifica si es hora de cambiar de dirección.
         timeUntilChange -= Time.deltaTime;
         if (timeUntilChange <= 0)
         {
@@ -50,16 +43,13 @@ public class RandomMovement : MonoBehaviour
 
     private void CambiarDireccion()
     {
-        // Genera una nueva dirección aleatoria en ambos ejes.
         currentDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
 
-        // Establece un nuevo tiempo para el próximo cambio de dirección.
         timeUntilChange = Random.Range(changeDirectionTime * 0.5f, changeDirectionTime * 1.5f);
     }
 
     private void OnDrawGizmos()
     {
-        // Dibuja una representación de los límites del área en el Editor de Unity.
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireCube(limitObject.position, new Vector3(limitObjectSize.x * 2, limitObjectSize.y * 2, 0));
     }
---
+++ GameJam+2023/Assets/GameJam/_Scripts/TeletransportPlayer_sc.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeletransportPlayer_sc : MonoBehaviour
+{
+    [SerializeField] Transform respawn; 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Void"))
+        {
+            TeletransportPlayer();
+        }
+    }
+    private void TeletransportPlayer()
+    {
+        if (respawn != null)
+        {
+            transform.position = respawn.position;
+        }
+    }
+}
